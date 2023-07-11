c31c150911df7bf3b385e6838f818d98a2d4f325
Victor Willian <victorwillian1012@gmail.com>
2023-07-10 21:19:40 -0300

    catppuccin theme and symbols outline

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,13 +1,4 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox with transparent background
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-      background = "transparent",
-    },
-  },
 }
